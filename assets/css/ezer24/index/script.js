d2245d591016fd98e5e0abb5dc43cb202597ef0e
Pearoo <sixtyny@gmail.com>
2023-12-11 10:36:20 +0100

    Add /ezer24

---
+++ assets/css/ezer24/index/script.js
@@ -0,0 +1,12 @@
+const subs = document.querySelector(".footer-subs")
+const bar = document.querySelector(".bar-accent")
+const subsbar = document.querySelector(".subs-bar")
+const goal = 1000;
+
+(async () => {
+    const api = await fetch("https://api.socialcounts.org/youtube-live-subscriber-count/UCsBw4YIB40Zpiw4yVIqxtbg")
+    const data = await api.json();
+    subs.innerText = `${data['API_sub']}/${goal}`
+    const barW = (data['API_sub'] * 100) / goal + "%"
+    bar.style.width = barW
+})();
