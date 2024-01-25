153def41054b0525dcadee1bcefce0cf5bb37790
Pearoo <sixtyny@gmail.com>
2024-01-25 22:05:22 +0100

    made sub counter pretty much automatic

--- assets/css/pearoo/index/script.js
+++ assets/css/pearoo/index/script.js
@@ -1,12 +1,12 @@
-const subs = document.querySelector(".footer-subs")
-const bar = document.querySelector(".bar-accent")
-const subsbar = document.querySelector(".subs-bar")
-const goal = 4000;
+const subs = document.querySelector(".footer-subs");
+const bar = document.querySelector(".bar-accent");
+const subsbar = document.querySelector(".subs-bar");
 
 (async () => {
-    const api = await fetch("https://api.socialcounts.org/youtube-live-subscriber-count/UCsBw4YIB40Zpiw4yVIqxtbg")
+    const api = await fetch("https://api.socialcounts.org/youtube-live-subscriber-count/UCsBw4YIB40Zpiw4yVIqxtbg");
     const data = await api.json();
-    subs.innerText = `${data['API_sub']}/${goal}`
-    const barW = (data['API_sub'] * 100) / goal + "%"
-    bar.style.width = barW
+    const goal = (Math.floor(data['API_sub']/1000)+1)*1000;
+    subs.innerText = `${data['API_sub']}/${goal}`;
+    const barW = (data['API_sub'] * 100) / goal + "%";
+    bar.style.width = barW;
 })();
